2b4f712cd1f14d58602965866f54f8df36df2f83
Abdelila Khossan <akhos1x@akhos1x.akhos1x>
2020-12-06 21:36:50 +0000

    first commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String [] args) {
+		ArrayList l = new ArrayList(1);
+		l.add(1);
+		l.add(1);
+		l.add(1);
+		System.out.println(l.size());
+	}
+}
